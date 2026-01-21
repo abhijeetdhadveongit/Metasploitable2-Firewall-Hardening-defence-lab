d8a0504b4626fc857a21a6fbcf706d4c8cfc5afd
Abhijeet Dhadve <r.d.abhijeet97@gmail.com>
2026-01-21 23:18:57 +0530

    .

--- firewall/iptables-setup.sh
+++ firewall/iptables-setup.sh
@@ -1,15 +1,17 @@
 ## iptables setup
-```bash
+
 iptables -F
 iptables -X
 iptables -t nat -F
 iptables -t mangle -F
+
 iptables -P INPUT DROP
 iptables -P FORWARD DROP
 iptables -P OUTPUT ACCEPT
+
 iptables -A INPUT -i lo -j ACCEPT
 iptables -A INPUT -m conntrack --ctstate ESTABLISHED,RELATED -j ACCEPT
 iptables -A INPUT -p icmp -j ACCEPT
 iptables -A INPUT -j LOG --log-prefix "DROP_INPUT: "
+
 iptables-save > /etc/iptables.rules
-```
